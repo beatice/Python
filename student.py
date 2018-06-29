c66b6b7c9e30830d1a8a07ebc1b7abf85bce945d
beatice <1979500428@qq.com>
2018-06-29 17:20:30 +0800

    Create student.py

---
+++ student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+number = int(input("Please enter the number of students: "))
+subjects = ('physics', 'math', 'history')
+student = []
+data = []
+
+for i in range(0, number):
+    name = input("Please enter the name of student {}:".format(i+1))
+    student.append(name)
+    mark = []
+    for x in subjects:
+        mark.append(input("Please enter {}'s {} grade(0-100): ".format(name, x)))
+    student.append(mark)
+    data.append(student)
+
+print("*" * 50)
+for i in range(0, number):
+    print("Student {}: {%10s}, Total grade: {%6d} ".format(i+1, data[i][0], sum(data[i][1]), end=' ')
+    if sum(data[i][1]) < 120:
+        print("failed")
+    else:
+        print("passed")
+
