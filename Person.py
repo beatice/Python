240b1e2a716dd2a2caddd7ca291ce2040e2149ea
beatice <1979500428@qq.com>
2018-07-05 14:24:07 +0800

    Update Person.py

--- Person.py
+++ Person.py
@@ -1,1 +1,39 @@
 #!/usr/bin/env python3
+
+class Person(object):
+    """ 返回具有给定名称的 Person 对象 """
+    
+    def __init__(self, name):
+        self.name = name
+    
+    def get_details(self):
+        return self.name
+
+class Student(Person):
+    """ 返回 Student 对象，采用 name, branch, year 3个参数 """
+    
+    def __init__(self, name, branch, year):
+        Person.__init__(self, name)
+        self.branch = branch
+        self.year = year
+    
+    def get_details(self):
+        return "{} studies {} and is in {} year.".format(self.name, self.branch, self.year)
+
+class Teacher(Person):
+    """ 返回 Teacher 对象，采用字符串列表作为参数 """
+    
+    def __init__(self, name, papers):
+        Person.__init__(self, name)
+        self.papers = papers
+        
+    def get_details(self):
+        return "{} teaches {}".format(self.name, ','.join(self.papers))
+      
+person1 = Person('Sam')
+student1 = Student('Amy', 'CSE', 2009)
+teacher1 = Teacher('John', ['Art', 'Computer', 'Management'])
+
+print(person1.get_details())
+print(student1.get_details())
+print(teacher1.get_details())
