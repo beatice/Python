5983441989aba1388efa8a072d2b2f19e4405545
beatice <1979500428@qq.com>
2018-07-06 13:58:04 +0800

    Update Person.py

--- Person.py
+++ Person.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import os
+from collections import Counter
 
 class Person(object):
     """ 返回具有给定名称的 Person 对象 """
@@ -8,6 +10,9 @@ class Person(object):
     
     def get_details(self):
         return self.name
+    
+    def get_grade(self, grade):
+        return self.grade
 
 class Student(Person):
     """ 返回 Student 对象，采用 name, branch, year 3个参数 """
@@ -19,6 +24,10 @@ class Student(Person):
     
     def get_details(self):
         return "{} studies {} and is in {} year.".format(self.name, self.branch, self.year)
+    
+    def get_grade(self, grade):
+        c = Counter(grade)
+        print("A: {}, 
 
 class Teacher(Person):
     """ 返回 Teacher 对象，采用字符串列表作为参数 """
@@ -29,6 +38,9 @@ class Teacher(Person):
         
     def get_details(self):
         return "{} teaches {}".format(self.name, ','.join(self.papers))
+    
+    def get_grade(self, grade):
+        
       
 person1 = Person('Sam')
 student1 = Student('Amy', 'CSE', 2009)
